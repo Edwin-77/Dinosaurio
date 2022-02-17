039acd5958976dd2d4d0ecc8e0bbb4aa2e079a1c
Edwin Martínez <99147597+Edwin-77@users.noreply.github.com>
2022-02-17 15:54:45 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+function jump(){
+  if (character.classList != "animate"){
+  character.classList.add("animate");
+  }
+  setTimeout(function(){
+    character.classList.remove("animate");
+  },900);
+}
+
+var checkDead = setInterval(function(){
+  var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+  var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+  if (blockLeft<20 && blockLeft>0 && characterTop>=130){
+    block.style.display = "flex";
+    alert("Has Perdido.");
+  }
+},10);
